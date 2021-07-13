7745d53c0b20b3a6525016a2b4f9a7acb789253a
Omar Santiago <omarkenosia@gmail.com>
2021-07-12 20:13:48 -0400

    Add files via upload

---
+++ Torch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Torch : MonoBehaviour
+{
+    public float minIntensity = 2.0f;
+    public float maxIntensity = 3.0f;
+    public float Timer = 0.1f;
+    private new Light light = null;
+
+    private void Start()
+    {
+        light = this.GetComponentInChildren<Light>();
+        InvokeRepeating("Flicker", Timer, Timer);
+    }
+    private void Flicker()
+    {
+        float R = Random.Range(minIntensity, maxIntensity);
+        light.intensity = R;
+    }
+}
